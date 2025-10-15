bef88eeb5b74cda752935fcf1023edd201315405
SMNava02 <sm02.edu@gmail.com>
2025-10-15 19:49:20 +1000

    Added schema validation and password encryption in signup route

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { connectToDB } = require("./config/database");
 const User = require("./models/user");
 
@@ -12,12 +13,21 @@ app.post("/signup", async (req, res) => {
     console.log(
       `Request receieved to add a new user: ${JSON.stringify(req.body)}`
     );
-    let newUser = new User(req.body);
+    //TODO: Validate the req.body using helper function
+    let { firstName, lastName, emailId, password } = req.body;
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    let newUser = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
     await newUser.save();
     res.send("User Added Successfully!");
   } catch (err) {
-    console.log(`Error in user add flow: ${err}`);
-    res.send(`Unable to add new user: ${err}`);
+    console.log(`Error in user add flow: ${err.message}`);
+    res.send(`Unable to add new user: ${err.message}`);
   }
 });
 
@@ -31,7 +41,7 @@ app.get("/user", async (req, res) => {
     console.log(
       `Unable to fetch user info for email: ${req.body.emailId} Error: ${err}`
     );
-    res.status(400).send(`Unable to find user info: ${err}`);
+    res.status(400).send(`Unable to find user info: ${err.message}`);
   }
 });
 
@@ -42,8 +52,8 @@ app.get("/feed", async (req, res) => {
     const userInfo = await User.find();
     res.send(userInfo);
   } catch (err) {
-    console.log(`Unable to fetch all users Error: ${err}`);
-    res.status(400).send(`Unable to find users: ${err}`);
+    console.log(`Unable to fetch all users Error: ${err.message}`);
+    res.status(400).send(`Unable to find users: ${err.message}`);
   }
 });
 
@@ -53,20 +63,30 @@ app.delete("/user", async (req, res) => {
     await User.findByIdAndDelete(userId);
     res.send("User deleted successfully");
   } catch (err) {
-    console.log(`Unable to delete user: ${err}`);
-    res.status(400).send(`Unable to delete user: ${err}`);
+    console.log(`Unable to delete user: ${err.message}`);
+    res.status(400).send(`Unable to delete user: ${err.message}`);
   }
 });
 
-app.patch("/user", async (req, res) => {
+app.patch("/user/:userId", async (req, res) => {
   try {
-    let userId = req.body.userId;
+    let userId = req.params.userId;
     console.log(`Updating user with Id: ${userId}`);
+    // Add allowded user fields to be updated
+    const ALLOWED_UPDATES = ["photoUrl", "about", "skills", "age"];
+    let isUpdateAllowed = Object.keys(req.body).every((value) =>
+      value.includes(ALLOWED_UPDATES)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error(
+        "Allowed fields for update are [photoUrl, about, skills, age]"
+      );
+    }
     await User.findByIdAndUpdate(userId, req.body);
     res.send("User updated successfully");
   } catch (err) {
-    console.log(`Unable to update user: ${err}`);
-    res.status(400).send(`Unable to update user: ${err}`);
+    console.log(`Unable to update user: ${err.message}`);
+    res.status(400).send(`Unable to update user: ${err.message}`);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,58 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-let userSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  age: Number,
-  email: String,
-  password: String,
-});
+let userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 100,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error(
+            "Pls provide a valid gender. Options are [male, female, others]"
+          );
+        }
+      },
+    },
+    email: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email ID is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+      default: "This is default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("Users", userSchema);
 
